b6d971b46a1eb470326c7a5be5967d98db4fc462
Daytuns <daytonbaldizon@gmail.com>
2025-07-22 03:24:37 -0600

    Added memix white logo and attribution to footer

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import Logo from "../components/Logo";
 
 const StyledFooterWrapper = styled.div`
   border: 5px solid #f05033;
@@ -11,26 +10,45 @@ const StyledFooterWrapper = styled.div`
   height: 50vh;
   border-radius: 25px;
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  gap: 32px;
+  justify-content: space-between;
 
   @media (max-width: 768px) {
     flex-direction: column;
   }
 `;
 
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const Attribution = styled.p`
+  color: rgba(255, 255, 255, 0.6);
+  font-size: 10px;
+  text-align: center;
+  margin: 0;
+  line-height: 1.3;
+`;
+
 const Footer = () => {
   return (
     <StyledFooterWrapper>
-      <Logo animated color="#FBF6FD" size={126} /> ❤️
-      <a href="https://comp-soc.com" target="_blank" rel="noreferrer">
+      <LogoContainer>
         <img
-          src="media/logos/comp-soc.png"
-          alt="CompSoc Logo"
-          style={{ height: "126px" }}
+          src="/media/logos/memix_white.svg"
+          alt="memix logo"
+          width={126}
+          height={126}
         />
-      </a>
+      </LogoContainer>
+      <Attribution>
+        Components adapted from Project Share with modifications for styling and
+        functionality
+      </Attribution>
     </StyledFooterWrapper>
   );
 };
